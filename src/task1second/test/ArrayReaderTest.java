ca63606ff86b32e81a01bffdcf32f0a5d392023b
N <no5110059@gmail.com>
2021-05-05 02:36:59 +0300

    commit 04.05.2021 task1secont (no test, no strem)+task2(.xml+.xsd)

---
+++ src/task1second/test/ArrayReaderTest.java
@@ -0,0 +1,12 @@
+package task1second.test;
+
+import org.testng.annotations.Test;
+
+
+
+class ArrayReaderTest {
+@Test
+
+    void read() {
+    }
+}
---
+++ src/task1second/test/ArrayServiceTest.java
@@ -0,0 +1,91 @@
+package task1second.test;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import task1second.entity.ArrayEntity;
+import task1second.service.ArrayService;
+import task1second.service.ArrayServiceImpl;
+
+
+
+public class ArrayServiceTest {
+    ArrayEntity entity;
+    ArrayService arrayService;
+
+@BeforeClass
+public void setEntity(){
+    entity=new ArrayEntity(new Integer[]{4,-10,8,12});
+    arrayService=new ArrayServiceImpl();
+}
+    @Test
+    public void testFindMax() {
+    int actual=arrayService.findMax(entity);
+    int expected=12;
+    Assert.assertEquals(actual, expected, 0);
+   }
+
+    @Test
+    public void testFindMin() {
+        int actual =arrayService.findMin(entity);
+        int expected =-10;
+        Assert.assertEquals(actual, expected);
+
+    }
+
+    @Test
+    public void testFindSum() {
+        int actual=arrayService.findSum(entity);
+        int expected=14;
+        Assert.assertEquals(actual, expected);
+
+    }
+
+    @Test
+    public void testFindMed() {
+        int actual=arrayService.findMed(entity);
+        int expected=3;
+        Assert.assertEquals(actual, expected);
+
+
+    }
+
+    @Test
+    public void testFindPosCount() {
+        int actual=arrayService.findPosCount(entity);
+        int expected=3;
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testFindNegCount() {
+        int actual=arrayService.findNegCount(entity);
+        int expected=1;
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testSelectionSort() {
+        ArrayEntity actual=new ArrayEntity(new Integer[]{4,-10,8,12});
+        ArrayEntity expected=new ArrayEntity(new Integer[]{-10, 4, 8, 12});
+        arrayService.selectionSort(expected);
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testBubbleSort() {
+        ArrayEntity actual=new ArrayEntity(new Integer[]{4,-10,8,12});
+        ArrayEntity expected=new ArrayEntity(new Integer[]{-10, 4, 8, 12});
+        arrayService.bubbleSort(expected);
+        Assert.assertEquals(actual, expected);
+
+    }
+
+    @Test
+    public void testGetSort() {
+        ArrayEntity actual=new ArrayEntity(new Integer[]{4,-10,8,12});
+        ArrayEntity expected=new ArrayEntity(new Integer[]{-10, 4, 8, 12});
+        arrayService.getSort(expected);
+        Assert.assertEquals(actual, expected);
+    }
+}
